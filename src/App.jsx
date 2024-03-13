d2cf84064177947498e0ba440ed91562fea45377
Ashish Pal <ashpal2203@gmail.com>
2024-03-13 18:09:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Canvas } from "@react-three/fiber"
+import { Model } from "./components/Trial"
+import { OrbitControls } from "@react-three/drei"
+import Experience from "./components/Experience"
+
+
+function App() {
+ 
+
+  return (
+<Canvas>
+  <Experience/>
+</Canvas>
+  )
+}
+
+export default App
---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import { OrbitControls } from '@react-three/drei'
+import React from 'react'
+import { Model } from './Trial'
+
+export default function Experience() {
+  return (
+   <>
+    <OrbitControls/>
+    <Model />
+    </>
+  )
+}
